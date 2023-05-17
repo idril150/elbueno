9871cbac50e4979c48caceccdb8e520ad9e6ab2e
TheJorkits <jlal_1999@hotmail.com>
2023-05-17 10:13:15 -0600

    a

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -29,5 +29,13 @@ class UserSeeder extends Seeder
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
         ])-> assignRole('manager');
+
+        User::create([
+            'name' => 'mairo',
+            'email' => 'mairo@manager.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ])-> assignRole('user');
     }
 }
