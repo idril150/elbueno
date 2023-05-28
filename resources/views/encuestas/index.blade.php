ab3f2b6799e0cb02e1c358029c01d6cec3dbfa67
victorM <ebibari1@gmail.com>
2023-05-28 13:04:07 -0600

    a

--- resources/views/encuestas/index.blade.php
+++ resources/views/encuestas/index.blade.php
@@ -8,26 +8,13 @@
     <a href="{{route('encuestas.create')}}">crear encuesta</a>
     <ul>
         @foreach($encuestas as $encuesta)
-
-        <div class="container">
-            <div class="grid grid-cols-2 gap-4">
-                <div class="bg-blue-100"><a href="{{route('encuestas.show',$encuesta->id)}}">{{$encuesta->name}}</a></div>
-                <div class="bg-blue-200"><form action="{{ route('encuestas.cambiarEstado', $encuesta->id) }}" method="POST">
-                    @csrf
-                        <input type="hidden" name="estado" value="0">        
-                    <button type="submit" class="button">Desactivar</button>
-                </form></div>
-              
-            </div>
-        </div>
-
-
-
-
-
+        <a href="{{route('encuestas.show',$encuesta->id)}}">{{$encuesta->name}}</a>
         
-        
-        
+        <form action="{{ route('encuestas.cambiarEstado', $encuesta->id) }}" method="POST">
+            @csrf
+                <input type="hidden" name="estado" value="0">        
+            <button type="submit">Desactivar</button>
+        </form>
     <ul>
     </ul>
     
