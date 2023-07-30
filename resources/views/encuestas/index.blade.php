8eafd6be68d82094d1552eb732a21600821a6f8e
victorM <ebibari1@gmail.com>
2023-07-30 17:50:07 -0600

    a

--- resources/views/encuestas/index.blade.php
+++ resources/views/encuestas/index.blade.php
@@ -13,7 +13,7 @@
                 <div class="grid grid-cols-12 p-6">
                     {{-- ingresar respuesta de pregunta abierta --}}
                     <div class="col-span-10 ">
-                        <a href="{{ route('responders.show', $encuesta->id) }}" class="text-white bg-blue-500 hover:bg-blue-700 font-bold py-2 px-4 rounded">
+                        <a href="{{ route('encuestas.show', $encuesta->id) }}" class="text-white bg-blue-500 hover:bg-blue-700 font-bold py-2 px-4 rounded">
                             {{ __($encuesta->name) }}
                         </a>
                     </div>
