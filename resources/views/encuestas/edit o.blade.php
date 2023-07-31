ffa5c33e1abfc47dee0d34db58d7f52db28d412e
TheJorkits <jlal_1999@hotmail.com>
2023-07-30 18:43:25 -0600

    a

--- resources/views/encuestas/edit o.blade.php
+++ resources/views/encuestas/edit o.blade.php
@@ -1,4 +1,9 @@
- {{-- <div class="min-h-screen bg-blue-100">
+@extends('layouts.plantilla')
+
+@section('title', 'edit')
+
+@section('content')
+    <div class="min-h-screen bg-blue-100">
         <div class="container mx-auto">
             <a href="{{ route('encuestas.show', $encuesta->id) }}" class="bg-blue-400 px-4 py-2 text-sm uppercase font-bold text-cyan-50 rounded-lg text-center">← Volver a la encuesta</a>
 
@@ -46,51 +51,4 @@
             </div>
         </div>
     </div>
-@endsection --}}
-
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Crear pregunta') }}
-        </h2>
-    </x-slot>
-
-
-    <div class="py-12">
-        <div class="max-w-2xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                
-                <form method="POST" action="{{ route('encuestas.show', $encuesta->id) }}">
-                    @method('put')
-                    @csrf                    
-                    <br>
-                    <div class="ml-4">      
-
-                        <div>
-                            <div class="grid grid-cols-12 ">
-                                {{-- ingresar nombre de la encuesta --}}
-                                <div class="col-span-10">
-                                    <x-input-label for="encuesta" :value="__('encuesta')" />
-                                    <x-text-input class="block mt-1 w-full" type="text" name="texto" :value="old('texto', $encuesta->name)" required />      
-                                    <br>                                  
-                                </div>
-                            </div>
-                        </div>  
-                        <div class="flex items-center justify-end mt-4">
-                            <div class="grid grid-cols-12 ">
-                               
-                                <div class="col-span-10">
-                                    <x-acept-button class="ml-4">
-                                        {{ __('guardar') }}
-                                    </x-acept-button>
-                                </div>
-                            </div>
-                               
-                        </div>
-                    </form>
-                    <br>
-    
-                </div>
-            </div>
-        </div>
- </x-app-layout>
+@endsection
