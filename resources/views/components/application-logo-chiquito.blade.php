839f5c6266a2353f494e4a0d5c0dc398ec23810f
TheJorkits <jlal_1999@hotmail.com>
2023-08-11 10:39:36 -0600

    a

--- resources/views/components/application-logo-chiquito.blade.php
+++ resources/views/components/application-logo-chiquito.blade.php
@@ -3,9 +3,7 @@
 </svg> --}}
 
 <div style="display: flex; justify-content: space-between;">
-    @unless(request()->routeIs('dashboard', 'profile.edit','responders.index','responders.show','preguntas.create'
-    ,'preguntas.edit','users.edit','users.index','encuestas.index','encuestas.show','encuestas.edit','encuestas.create'
-    ,'respuestas.create','repuestas.edit'))
+    @unless(request()->routeIs(''))
     <a href="/">
         <img src="{{ asset('img/logo.png') }}" alt="Logo" style="width: 50px;">
     </a>
